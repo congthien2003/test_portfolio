527f0eaa7e7a24edc78b2995fd4b07cead42a133
CongThien2003 <conggthien17@gmail.com>
2024-04-25 15:58:12 +0700

    fix

--- app.js
+++ app.js
@@ -13,3 +13,33 @@ menu.addEventListener("click", () => {
 		closeIcon.style.display = "none";
 	}
 });
+
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".item-link");
+
+window.onscroll = () => {
+	sections.forEach((section) => {
+		let top = window.scrollY;
+		let offset = section.offsetTop;
+		let height = section.offsetHeight;
+
+		let id = section.getAttribute("id");
+		if (top + 100 >= offset && top < offset + height) {
+			navItems.forEach((nav) => {
+				nav.classList.remove("active");
+				document
+					.querySelector("header nav a[href*=" + id + "]")
+					.classList.add("active");
+			});
+		}
+		// Đặt khoảng cách từ đầu trang mà tại đó sẽ xóa active
+		const deactivateThreshold = 0; // Ví dụ: 50px
+
+		// Kiểm tra nếu vị trí cuộn nhỏ hơn ngưỡng, xóa lớp active
+		if (top <= deactivateThreshold) {
+			navItems.forEach((nav) => {
+				nav.classList.remove("active");
+			});
+		}
+	});
+};
